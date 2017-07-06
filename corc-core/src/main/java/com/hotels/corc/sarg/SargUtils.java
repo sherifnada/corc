2118feeeec7ecfab0c9876e547d17c8c1ebc6f05
Shrif Nada <shrif.nada@liveramp.com>
2017-07-05 22:21:55 -0700

    actually add SargUtils code

--- corc-core/src/main/java/com/hotels/corc/sarg/SargUtils.java
+++ corc-core/src/main/java/com/hotels/corc/sarg/SargUtils.java
@@ -1,4 +1,33 @@
 package com.hotels.corc.sarg;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.hadoop.hive.ql.io.sarg.SearchArgument;
+import org.apache.hadoop.hive.ql.io.sarg.SearchArgumentImpl;
+
 public class SargUtils {
+  private static int BUFFER_SIZE = 4 * 1024;
+  private static final int MAX_BUFFER_SIZE = 10 * 1024 * 1024;
+
+  /**
+   * @param sarg
+   * @return
+   */
+  public static String toKryo(SearchArgument sarg) {
+    Output out = new Output(BUFFER_SIZE, MAX_BUFFER_SIZE);
+    new Kryo().writeObject(out, sarg);
+    out.close();
+    return Base64.encodeBase64String(out.toBytes());
+  }
+
+  /**
+   * @param kryo Base64 String encoding of the Kryo-serialized object
+   * @return the {@link SearchArgument} object represented by the argument
+   */
+  public static SearchArgument fromKryo(String kryo) {
+    Input input = new Input(Base64.decodeBase64(kryo));
+    return new Kryo().readObject(input, SearchArgumentImpl.class);
+  }
 }
